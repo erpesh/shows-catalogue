d50441d9398c5fe62837450c02a9962f8a3b061f
Anton <antonio.fiasco24@gmail.com>
2024-01-08 14:24:17 +0000

    removed Person classes, left Actor

--- Catalogue/Models/Actor.cs
+++ Catalogue/Models/Actor.cs
@@ -6,13 +6,50 @@ using System.Threading.Tasks;
 
 namespace Catalogue.Models
 {
-    public class Actor : Person
+    public class Actor
     {
-        public Actor(string _firstName, string _lastName, DateOnly _dateOfBirth, string _nationality)
-            : base(_firstName, _lastName, _dateOfBirth, _nationality)
+        protected int id;
+        protected string fullName;
+        protected DateOnly dateOfBirth;
+        protected string nationality;
+        protected List<string> filmography;
+
+        public int Id
         {
+            get => id;
+            set => id = value;
         }
 
-    }
+        public string FullName
+        {
+            get => fullName;
+            set => fullName = value;
+        }
 
+        public DateOnly DateOfBirth
+        {
+            get => dateOfBirth;
+            set => dateOfBirth = value;
+        }
+
+        public string Nationality
+        {
+            get => nationality;
+            set => nationality = value;
+        }
+
+        public List<string> Filmography
+        {
+            get => filmography; 
+            set => filmography = value; 
+        }
+
+        public Actor(string _fullName, DateOnly _dateOfBirth, string _nationality, List<string> filmography)
+        {
+            FullName = _fullName;
+            DateOfBirth = _dateOfBirth;
+            Nationality = _nationality;
+            Filmography = filmography;
+        }
+    }
 }
